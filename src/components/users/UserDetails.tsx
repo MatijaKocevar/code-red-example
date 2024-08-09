7ed294c972552a83b2d7cca52e315de5295715a4
Matija Kočevar <matija.kocev@gmail.com>
2024-08-09 18:21:30 +0200

    Create UserDetails component to de-clutter code

---
+++ src/components/users/UserDetails.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { User } from "../../types/user";
+
+interface UserDetailsProps {
+    user: User | null;
+}
+
+const UserDetails: React.FC<UserDetailsProps> = ({ user }) => {
+    if (!user) {
+        return <div className="text-gray-500">Select a user to see the details</div>;
+    }
+
+    return (
+        <div className="bg-white p-4 shadow-md rounded-md border border-gray-200 h-full">
+            <h2 className="text-xl font-bold mb-4 text-gray-800">User Details</h2>
+            <p className="text-gray-500">
+                <strong>ID:</strong> {user.uuid}
+            </p>
+            <p className="text-gray-500">
+                <strong>First Name:</strong> {user.firstName}
+            </p>
+            <p className="text-gray-500">
+                <strong>Last Name:</strong> {user.lastName}
+            </p>
+        </div>
+    );
+};
+
+export default UserDetails;
