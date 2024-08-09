0f1d13faaf234640d032ecba442f3e0021f5a0de
Matija Kočevar <matija.kocev@gmail.com>
2024-08-09 18:04:47 +0200

    Fix MainLayout to correctly display the page height

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,33 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import { useEffect, useRef, useState } from "react";
 const MainLayout = () => {
+    const [navBarHeight, setNavBarHeight] = useState<number>(0);
+    const navBarRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (navBarRef.current) {
+            setNavBarHeight(navBarRef.current.clientHeight);
+        }
+
+        const handleResize = () => {
+            if (navBarRef.current) {
+                setNavBarHeight(navBarRef.current.clientHeight);
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
-        <div>
-            <NavBar />
+        <div className={`h-[calc(100%-${navBarHeight}px)]`}>
+            <div ref={navBarRef}>
+                <NavBar />
+            </div>
             <Outlet />
         </div>
     );
