50933d9d597bac04a9c3d0bd1401a682f433f903
Matija Kočevar <matija.kocev@gmail.com>
2024-08-09 21:34:30 +0200

    Update EventsPage component

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,5 +1,33 @@
-const EventsPage = () => {
-    return <div>Events Page</div>;
+import React from "react";
+import { useEvents } from "../hooks/useEvents";
+import EventList from "../components/events/EventList";
+import EventForm from "../components/events/EventForm";
+
+const EventsPage: React.FC = () => {
+    const {
+        events,
+        title,
+        probability,
+        loading,
+        error,
+        setTitle,
+        setProbability,
+        handleDelete,
+        handleAddEvent,
+    } = useEvents();
+
+    return (
+        <div className="flex gap-4 h-full p-8">
+            <EventList events={events} loading={loading} error={error} onDelete={handleDelete} />
+            <EventForm
+                title={title}
+                probability={probability}
+                onTitleChange={(e) => setTitle(e.target.value)}
+                onProbabilityChange={(e) => setProbability(parseInt(e.target.value))}
+                onAddEvent={handleAddEvent}
+            />
+        </div>
+    );
 };
 
 export default EventsPage;
