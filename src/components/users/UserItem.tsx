17359e311bc4ab7445e2214e961a8066df537d88
Matija Kočevar <matija.kocev@gmail.com>
2024-08-09 18:21:14 +0200

    Create UserItem component to de-clutter code

---
+++ src/components/users/UserItem.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { User } from "../../types/user";
+
+interface UserItemProps {
+    user: User;
+    onClick: (user: User) => void;
+}
+
+const UserItem: React.FC<UserItemProps> = ({ user, onClick }) => {
+    return (
+        <li
+            className="p-4 bg-white shadow-md rounded-md border border-gray-200 cursor-pointer"
+            onClick={() => onClick(user)}
+        >
+            <div className="flex items-center space-x-4">
+                <div className="text-lg font-medium text-gray-700">
+                    {user.firstName} {user.lastName}
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default UserItem;
