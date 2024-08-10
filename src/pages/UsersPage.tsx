851bc4855a99b5f9f1efc72d8f056f9403767755
Matija Kočevar <matija.kocev@gmail.com>
2024-08-10 16:45:45 +0200

    Create and implement useUsers hook

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,49 +1,30 @@
-import React, { useEffect, useState } from "react";
-import useApi from "../hooks/useApi";
+import React from "react";
 import UserDetails from "../components/users/UserDetails";
-import { User } from "../types/user";
+import { useUsers } from "../hooks/useUsers";
 import CustomList from "../components/CustomList";
 import UserItem from "../components/users/UserItem";
+import { User } from "../types/user";
 
 const UsersPage: React.FC = () => {
-    const [users, setUsers] = useState<User[]>([]);
-    const [selectedUser, setSelectedUser] = useState<User | null>(null);
-    const { loading, error, request } = useApi();
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const data = await request<User[]>("user");
-
-            if (data) {
-                setUsers(data);
-            } else {
-                console.error("Failed to fetch users");
-            }
-        };
-
-        fetchUsers();
-    }, [request]);
-
-    const handleUserClick = (user: User) => {
-        setSelectedUser(user);
-    };
+    const { users, selectedUser, loading, error, handleUserClick } = useUsers();
 
     if (loading) return <div className="m-4">Loading...</div>;
     if (error) return <div className="m-4 text-red-500">Error: {error}</div>;
 
     return (
         <div className="h-full flex gap-8 p-8">
-            <CustomList<User>
-                items={users}
-                loading={loading}
-                error={error}
-                onDelete={() => {}}
-                itemKey={(user) => user.uuid}
-                renderItem={(user) => (
-                    <UserItem key={user.uuid} user={user} onClick={handleUserClick} />
-                )}
-            />
-
+            <div className="w-1/2 h-full">
+                <CustomList<User>
+                    items={users}
+                    loading={loading}
+                    error={error}
+                    onDelete={() => {}}
+                    itemKey={(user) => user.uuid}
+                    renderItem={(user) => (
+                        <UserItem key={user.uuid} user={user} onClick={handleUserClick} />
+                    )}
+                />
+            </div>
             <div className="w-1/2 h-full">
                 <UserDetails user={selectedUser} />
             </div>
