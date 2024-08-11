d8c14597742ad529eab4cfdac3922655711b19ea
Matija Kočevar <matija.kocev@gmail.com>
2024-08-11 19:45:34 +0200

    Fix base path for github pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import PostsPage from "./pages/PostsPage";
 import EventsPage from "./pages/EventsPage";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import UsersPage from "./pages/UsersPage";
 
-const baseUrl = import.meta.env.VITE_BASE_URL;
-
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <MainLayout />,
-            children: [
-                { index: true, element: <HomePage /> },
-                { path: "users", element: <UsersPage /> },
-                { path: "posts", element: <PostsPage /> },
-                { path: "events", element: <EventsPage /> },
-            ],
-        },
-    ],
-    { basename: baseUrl }
-);
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: "users", element: <UsersPage /> },
+            { path: "posts", element: <PostsPage /> },
+            { path: "events", element: <EventsPage /> },
+        ],
+    },
+]);
 
 export default router;
